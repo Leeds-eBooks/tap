475b2b6ae0d3ac4cbdbaedfbbb82703738967da7
Ben Styles <enquiries@leeds-ebooks.co.uk>
2014-12-26 13:04:35 +0000

    Built to dist using grunt

--- dist/tap.legacy.js
+++ dist/tap.legacy.js
@@ -1,28 +1,6 @@
 (function( window ) {
     var Tap = {};
 
-    var utils = {};
-
-    utils.attachEvent = function( element, eventName, callback ) {
-        return element.addEventListener( eventName, callback, false );
-    };
-
-    utils.fireFakeEvent = function( e, eventName ) {
-        return e.target.dispatchEvent( utils.createEvent( eventName ) );
-    };
-
-    utils.createEvent = function( name ) {
-        var evnt = window.document.createEvent( 'HTMLEvents' );
-        evnt.initEvent( name, true, true );
-        evnt.eventName = name;
-
-        return evnt;
-    };
-
-    utils.getRealEvent = function( e ) {
-        return e.originalEvent && e.originalEvent.touches && e.originalEvent.touches.length ? e.originalEvent.touches[ 0 ] : e;
-    };
-
     var eventMatrix = [{
         // Touchable devices
         test: ( 'propertyIsEnumerable' in window || 'hasOwnProperty' in document ) && ( window.propertyIsEnumerable( 'ontouchstart' ) || document.hasOwnProperty( 'ontouchstart') ),
